9bdacc5e7dd0b2bcb4af055f51d4242f3442ae5a
Guido Schmidt <git@guidoschmidt.cc>
2025-01-16 09:42:36 +0100

    fix(2024): c_allocator → page_allocator

--- 2024/day11.zig
+++ 2024/day11.zig
@@ -124,7 +124,7 @@ test "simple test" {
 }
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
