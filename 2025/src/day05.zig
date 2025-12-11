0e113c544aa0ed8f6ceb686cce0bedb3d7e38f3c
Guido Schmidt <git@guidoschmidt.cc>
2025-12-11 12:37:00 +0100

    feat(2025/day05): solve part 1 and part 2

--- 2025/src/day05.zig
+++ 2025/src/day05.zig
@@ -7,9 +7,7 @@ const DAY: u5 = 5;
 const Allocator = std.mem.Allocator;
 const log = std.log;
 
-fn part1(allocator: Allocator, input: []const u8) anyerror!void {
-    _ = input;
-
+fn part1(allocator: Allocator) anyerror!void {
     var fresh_ingredients: usize = 0;
 
     const input_embed = @embedFile("puzzle-05");
@@ -42,28 +40,56 @@ fn part1(allocator: Allocator, input: []const u8) anyerror!void {
     std.debug.print("Result: {d}\n", .{fresh_ingredients});
 }
 
-fn part2(allocator: Allocator, input: []const u8) anyerror!void {
-    _ = input;
-    // _ = allocator;
-
-    // var fresh_ingredients: usize = 0;
-
-    const input_embed = @embedFile("example-05");
+fn part2(allocator: Allocator) anyerror!void {
+    const input_embed = @embedFile("puzzle-05");
     const split_at = std.mem.indexOf(u8, input_embed, "\n\n").?;
     const ranges = std.mem.trim(u8, input_embed[0..split_at], "\n");
 
     var ranges_list: std.array_list.Managed(@Vector(2, usize)) = .init(allocator);
     defer ranges_list.deinit();
 
-    var sum: usize = 0;
     var ranges_reader: std.Io.Reader = .fixed(ranges);
     while (try ranges_reader.takeDelimiter('\n')) |range| {
         const sep = std.mem.indexOf(u8, range, "-").?;
         const lower = try std.fmt.parseInt(usize, range[0..sep], 10);
         const upper = try std.fmt.parseInt(usize, range[sep + 1 ..], 10);
+
         try ranges_list.append(.{ lower, upper });
-        std.debug.print("\n{d}", .{upper - lower});
-        sum += upper - lower;
+    }
+
+    std.mem.sort(@Vector(2, usize), ranges_list.items, {}, comptime struct {
+        pub fn f(_: void, a: @Vector(2, usize), b: @Vector(2, usize)) bool {
+            return a[0] < b[0];
+        }
+    }.f);
+
+    for (0..ranges_list.items.len - 1) |r| {
+        const curr = ranges_list.items[r];
+        const next = ranges_list.items[r + 1];
+
+        // Current range overlaps next range on lower bound of next
+        if (curr[1] >= next[0]) {
+
+            // Extend next range on lower end
+            ranges_list.items[r + 1][0] = curr[0];
+
+            // Current range overlaps next range on upper bound of next
+            if (curr[1] > next[1]) {
+                // Extend next range on upper end
+                ranges_list.items[r + 1][1] = curr[1];
+            }
+
+            // Dismiss current range
+            // (as the next range has been expanded and ids shouldn't be
+            // counted twice)
+            ranges_list.items[r] = .{ 0, 0 };
+        }
+    }
+
+    var sum: usize = 0;
+    for (ranges_list.items) |r| {
+        if (r[0] == 0 or r[1] == 0) continue;
+        sum += r[1] - r[0] + 1;
     }
 
     std.debug.print("Result: {d}\n", .{sum});
@@ -74,6 +100,6 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    // try aoc.runPart(allocator, DAY, .EXAMPLE, part1);
-    try aoc.runPart(allocator, DAY, .EXAMPLE, part2);
+    try aoc.runPart(allocator, part1);
+    try aoc.runPart(allocator, part2);
 }
