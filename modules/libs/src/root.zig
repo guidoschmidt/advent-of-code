97ac2f8f85f4e9e13bfcbb669b0081551f6ad2f9
Guido Schmidt <git@guidoschmidt.cc>
2025-12-15 22:06:25 +0100

    feat(2025): solve day 09, part 1 + 2

--- modules/libs/src/root.zig
+++ modules/libs/src/root.zig
@@ -1,4 +1,5 @@
 pub const VectorSet = @import("datastructures/VectorSet.zig").VectorSet;
+pub const Map = @import("datastructures/Map.zig").Map;
 pub const math = @import("math.zig");
 pub const ppm = @import("ppm.zig");
 pub const svg = @import("svg.zig");
