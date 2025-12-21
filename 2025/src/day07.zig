2b797e7d8d38fb16bee2b9b72bc090872d4c19b9
Guido Schmidt <git@guidoschmidt.cc>
2025-12-21 12:52:13 +0100

    feat(2025/07): start with part 2

--- 2025/src/day07.zig
+++ 2025/src/day07.zig
@@ -1,54 +1,23 @@
 const std = @import("std");
 const aoc = @import("aoc");
+const t = @import("libs").term;
+const Map = @import("libs").Map;
 
 const DAY: u5 = 7;
 
 const Allocator = std.mem.Allocator;
 const log = std.log;
 
-const Map = struct {
-    rows: usize,
-    cols: usize,
-    buffer: []u8,
-
-    pub fn init(allocator: Allocator, buffer: []const u8) !Map {
-        if (std.mem.indexOf(u8, buffer, "\n")) |line_end| {
-            return .{
-                .buffer = try std.mem.replaceOwned(u8, allocator, buffer, "\n", ""),
-                .cols = line_end,
-                .rows = (buffer.len - 1) / line_end,
-            };
-        }
-        @panic("Failed to split input buffer!");
-    }
-
-    pub fn get(self: *const Map, x: usize, y: usize) u8 {
-        return self.buffer[(y * self.cols) + x];
-    }
-
-    pub fn format(self: Map, writer: *std.Io.Writer) std.Io.Writer.Error!void {
-        try writer.print("Map {d} x {d}\n", .{ self.rows, self.cols });
-        for (0..self.rows) |y| {
-            for (0..self.cols) |x| {
-                try writer.print("{c}", .{self.get(x, y)});
-            }
-            try writer.print("\n", .{});
-        }
-    }
-
-    pub fn deinit(self: Map, allocator: Allocator) void {
-        allocator.free(self.buffer);
-    }
-};
-
 fn part1(allocator: Allocator) anyerror!void {
-    const input = @embedFile("example-07");
-    std.debug.print("--- INPUT---\n{s}\n------------\n", .{input});
+    const input = @embedFile("puzzle-07");
+    // std.debug.print("--- INPUT---\n{s}\n------------\n", .{input});
 
     const map: Map = try .init(allocator, input);
 
     var start: @Vector(2, usize) = @splat(0);
     var splitters: std.array_list.Managed(@Vector(2, usize)) = .init(allocator);
+
+    // std.debug.print("{d} x {d}\n", .{ map.cols, map.rows });
 
     for (0..map.rows) |y| {
         for (0..map.cols) |x| {
@@ -59,12 +28,103 @@ fn part1(allocator: Allocator) anyerror!void {
             }
         }
     }
+
+    var beams: std.array_list.Managed(@Vector(2, usize)) = .init(allocator);
+    try beams.append(start);
+    var splits: usize = 0;
+    while (beams.pop()) |b| {
+        std.mem.sort(@Vector(2, usize), beams.items, {}, comptime struct {
+            pub fn f(_: void, beam1: @Vector(2, usize), beam2: @Vector(2, usize)) bool {
+                return beam1[1] > beam2[1];
+            }
+        }.f);
+
+        map.animate();
+        const curr: @Vector(2, usize) = .{ b[0], b[1] };
+        if (curr[0] < 0 or
+            curr[0] > map.cols - 1 or
+            curr[1] > map.rows - 1)
+        {
+            continue;
+        }
+        switch (map.get(curr[0], curr[1])) {
+            'S' => try beams.append(.{ curr[0], curr[1] + 1 }),
+            '.' => try beams.append(.{ curr[0], curr[1] + 1 }),
+            '^' => {
+                splits += 1;
+                try beams.append(.{ curr[0] - 1, curr[1] });
+                try beams.append(.{ curr[0] + 1, curr[1] });
+            },
+            else => continue,
+        }
+        map.set(curr[0], curr[1], '|');
+    }
+    // std.debug.print("{f}\n", .{map});
+
+    std.debug.print("# Splitters: {d}\n", .{splitters.items.len});
+    std.debug.print("Result: {d}\n", .{splits});
 }
 
 fn part2(allocator: Allocator) anyerror!void {
-    _ = allocator;
     const input = @embedFile("puzzle-07");
-    std.debug.print("--- INPUT---\n{s}\n------------\n", .{input});
+    // std.debug.print("--- INPUT---\n{s}\n------------\n", .{input});
+
+    const map: Map = try .init(allocator, input);
+
+    var start: @Vector(2, usize) = @splat(0);
+    var splitters: std.array_list.Managed(@Vector(2, usize)) = .init(allocator);
+
+    // std.debug.print("{d} x {d}\n", .{ map.cols, map.rows });
+
+    for (0..map.rows) |y| {
+        for (0..map.cols) |x| {
+            switch (map.get(x, y)) {
+                'S' => start = .{ x, y },
+                '^' => try splitters.append(.{ x, y }),
+                else => continue,
+            }
+        }
+    }
+
+    var beams: std.array_list.Managed(@Vector(2, usize)) = .init(allocator);
+    try beams.append(start);
+    var timelines: usize = 1;
+    while (beams.pop()) |b| {
+        std.mem.sort(@Vector(2, usize), beams.items, {}, comptime struct {
+            pub fn f(_: void, beam1: @Vector(2, usize), beam2: @Vector(2, usize)) bool {
+                return beam1[0] > beam2[0];
+            }
+        }.f);
+
+        const curr: @Vector(2, usize) = .{ b[0], b[1] };
+
+        if (curr[0] < 0 or
+            curr[0] > map.cols - 1 or
+            curr[1] > map.rows - 1)
+        {
+            continue;
+        }
+
+        // const orig = map.get(curr[0], curr[1]);
+        // map.set(curr[0], curr[1], '|');
+        // map.animate();
+        // map.set(curr[0], curr[1], orig);
+
+        switch (map.get(curr[0], curr[1])) {
+            'S' => try beams.append(.{ curr[0], curr[1] + 1 }),
+            '.' => try beams.append(.{ curr[0], curr[1] + 1 }),
+            '^' => {
+                timelines += 1;
+                try beams.append(.{ curr[0] - 1, curr[1] });
+                try beams.append(.{ curr[0] + 1, curr[1] });
+            },
+            else => continue,
+        }
+    }
+    // std.debug.print("{f}\n", .{map});
+
+    std.debug.print("# Splitters: {d}\n", .{splitters.items.len});
+    std.debug.print("Result: {d}\n", .{timelines});
 }
 
 pub fn main() !void {
