c892f2a1e5afb88ea3563596646d7c69e49a9fbd
Guido Schmidt <git@guidoschmidt.cc>
2025-12-21 12:53:34 +0100

    feat(2025/11): start with day 11 part 2

--- 2025/src/day11.zig
+++ 2025/src/day11.zig
@@ -18,8 +18,26 @@ const Node = struct {
     }
 };
 
+const NodePath = struct {
+    path: std.array_list.Managed(*Node),
+
+    pub fn init(allocator: Allocator, start: *Node) !NodePath {
+        var node_path: NodePath = .{
+            .path = .init(allocator),
+        };
+        try node_path.path.append(start);
+        return node_path;
+    }
+
+    pub fn format(self: NodePath, writer: *std.Io.Writer) std.Io.Writer.Error!void {
+        for (self.path.items) |p| {
+            try writer.print("{s} → ", .{p.name});
+        }
+    }
+};
+
 fn part1(allocator: Allocator) anyerror!void {
-    const input = @embedFile("puzzle-11");
+    const input = @embedFile("example-11");
     std.debug.print("--- INPUT---\n{s}\n------------\n", .{input});
 
     var device_map: std.hash_map.StringHashMap(Node) = .init(allocator);
@@ -63,9 +81,55 @@ fn part1(allocator: Allocator) anyerror!void {
 }
 
 fn part2(allocator: Allocator) anyerror!void {
-    _ = allocator;
     const input = @embedFile("example-11");
     std.debug.print("--- INPUT---\n{s}\n------------\n", .{input});
+
+    var device_map: std.hash_map.StringHashMap(Node) = .init(allocator);
+    defer device_map.deinit();
+
+    var reader: std.Io.Reader = .fixed(input);
+    while (try reader.takeDelimiter('\n')) |line| {
+        const colon = std.mem.indexOf(u8, line, ":") orelse @panic(": not found");
+        const device_name = line[0..colon];
+        const connections = line[colon + 1 ..];
+        var connections_it = std.mem.splitSequence(u8, connections, " ");
+        var node: Node = .{
+            .name = device_name,
+            .connections = .init(allocator),
+        };
+        while (connections_it.next()) |connection| {
+            if (connection.len == 0) continue;
+            try node.connections.append(connection);
+        }
+        try device_map.put(device_name[0..3], node);
+    }
+
+    var paths: std.array_list.Managed(NodePath) = .init(allocator);
+    defer paths.deinit();
+
+    var stack: std.array_list.Managed(*Node) = .init(allocator);
+    defer stack.deinit();
+    try stack.append(device_map.getPtr("you").?);
+
+    while (stack.pop()) |next| {
+        if (std.mem.eql(u8, next.name, "out")) break;
+        std.debug.print("[{d}] Next: {f}\n", .{ stack.items.len, next });
+        if (next.connections.items.len > 0) {
+            if (device_map.getPtr(next.connections.items[0])) |ptr| {
+                try stack.append(ptr);
+            }
+        }
+
+        // for (next.connections.items) |connection| {
+        //     if (std.mem.eql(u8, connection, "out")) {
+        //         result += 1;
+        //         continue;
+        //     }
+        //     if (device_map.getPtr(connection)) |ptr| {
+        //         try stack.append(ptr);
+        //     }
+        // }
+    }
 }
 
 pub fn main() !void {
@@ -74,5 +138,5 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     try aoc.runPart(allocator, part1);
-    // try aoc.runPart(allocator, part2);
+    try aoc.runPart(allocator, part2);
 }
